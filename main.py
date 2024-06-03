df029ef1d09055bdc71876b4ccaf476924b47828
prajun v <prajunvelandi@gmail.com>
2024-06-03 21:12:20 +0530

    added the comments to make the code readable

--- main.py
+++ main.py
@@ -16,10 +16,12 @@ class Class2(Thread):
 obj1 = Class1()
 obj2 = Class2()
 
+# method start inside the Thread class is used to perform multi threading
 obj1.start()
 sleep(0.2)
 obj2.start()
 
+#joining the threads
 obj1.join()
 obj2.join()
 
